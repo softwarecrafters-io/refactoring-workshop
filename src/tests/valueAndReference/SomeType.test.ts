13ab93dfd479f866007e534d989c8ad877dbe062
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-11-04 17:09:51 +0000

    green: You should understand values and references because

--- src/tests/valueAndReference/SomeType.test.ts
+++ src/tests/valueAndReference/SomeType.test.ts
@@ -5,24 +5,24 @@ describe('You should understand values and references because', ()=>{
 		const instance = new SomeType();
 		const arg1 = 'bar';
 		instance.firstMethod(arg1);
-		expect(arg1).toBe('????')
+		expect(arg1).toBe('bar')
 
 		const other = new SomeType();
 		other.numbers[0] = 100;
 		other.secondMethod(other)
-		expect(other.numbers[0]).toBe('????')
-		expect(other.someField).toBe('????')
+		expect(other.numbers[0]).toBe(100)
+		expect(other.someField).toBeUndefined()
 
 		const another: SomeType = new SomeType();
 		another.numbers[0] = 200;
 		instance.thirdMethod(another);
-		expect(another.numbers[0]).toBe('????');
-		expect(another.someField).toBe("????");
-		expect(another.child).toBe('????');
+		expect(another.numbers[0]).toBe(100);
+		expect(another.someField).toBe("changed");
+		expect(another.child).toBeInstanceOf(SomeType)
 
 		const yetAnother: SomeType = new SomeType();
 		yetAnother.numbers[0] = 200;
 		instance.fourthMethod(yetAnother.numbers);
-		expect(yetAnother.numbers[0]).toBe('????');
+		expect(yetAnother.numbers[0]).toBe(0);
 	})
 })
